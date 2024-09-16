cae111eaa9f3e233325742a94f414455e2e61cf9
cj7mo <cj7mo@outlook.com>
2024-09-16 11:15:30 -0500

    export

--- data/index.ts
+++ data/index.ts
@@ -52,7 +52,7 @@ export const navItems = [
       title: "Working on Data Science & Machine Learning projects",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-50 rounded-lg sm:w-5 xs:w-1",
+      imgClassName: "absolute right-0 bottom-0 w-[10%] sm:w-auto",
       titleClassName: "justify-center md:justify-start lg:justify-center",
       img: "b5.png",
       spareImg: "grid.svg",
