20a426add045b10422f2ce2fbc83ead7ea1e74d6
cj7mo <cj7mo@outlook.com>
2024-09-16 10:04:44 -0500

    export

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data-science.png" sizes="any" />
+        <link rel="icon" href="data-science.svg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -191,7 +191,7 @@ export const BentoGridItem = ({
             </div>
           )}
           {id === 6 && (
-            <div className="mt-5 relative flex space-x-2 sm:flex-row sm:space-x-2 sm:space-y-0">
+            <div className="mt-5 relative flex flex-col sm:flex-row sm:space-x-2 space-y-2 sm:space-y-0 justify-center items-center">
       
             {showCopyConfetti && (
               <div className="absolute -bottom-5 right-0">
